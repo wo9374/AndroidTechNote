9a9aad09ad7e676c1c59be93bfba97756a284c75
jblee9374 <55440637+wo9374@users.noreply.github.com>
2022-11-17 11:27:46 +0900

    feat: CustomFocusView 구성용 Data Api 추가

---
+++ app/src/main/java/com/example/androidtechnote/recycler/custom_focus/data/FocusItem.kt
@@ -0,0 +1,22 @@
+package com.example.customlibrary
+
+import com.google.gson.annotations.SerializedName
+
+data class FocusItem(
+    @SerializedName("id") val id : Long,
+    @SerializedName("title") val title : String,
+    @SerializedName("overview") val overview : String,
+    @SerializedName("poster_path") val poster_path: String,
+    @SerializedName("backdrop_path") val backdrop_path: String,
+    @SerializedName("vote_average") val rating: Float,
+    @SerializedName("vote_count") val vcount: Long,
+    @SerializedName("release_date") val releaseDate: String,
+    @SerializedName("popularity") val prating: Float
+) {}
+
+data class GetFocusItemResponse(
+    @SerializedName("page") val page: Int,
+    @SerializedName("results") val movies: List<FocusItem>,
+    @SerializedName("totla_pages") val pages: Int,
+    @SerializedName("totla_results") val results: Int
+) {}
---
+++ app/src/main/java/com/example/androidtechnote/recycler/custom_focus/data/TMDBMoviesApi.kt
@@ -0,0 +1,15 @@
+package com.example.customlibrary
+
+import com.example.androidtechnote.BuildConfig
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBMoviesApi {
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEYS,
+        @Query("page") page : Int,
+        @Query("language") language : String = "ko" //"ko,en-US"
+    ): Response<GetFocusItemResponse>
+}
