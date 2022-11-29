d3ec3d688c97db7a5918335958f861de68a73f6d
jblee9374 <55440637+wo9374@users.noreply.github.com>
2022-11-29 17:12:08 +0900

    feat: BLE Scan List 구현

---
+++ app/src/main/java/com/example/androidtechnote/ble/BleListAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.androidtechnote.ble
+
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidtechnote.R
+import com.example.androidtechnote.databinding.ItemBleListBinding
+
+class BleListAdapter : ListAdapter<BluetoothDevice, BleListViewHolder>(DiffUtil()){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BleListViewHolder {
+        val binding = ItemBleListBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        return BleListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BleListViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class BleListViewHolder(val binding: ItemBleListBinding): RecyclerView.ViewHolder(binding.root){
+
+    val context : Context
+        get() = itemView.context
+
+    fun bind(device: BluetoothDevice){
+        binding.apply {
+            deviceName.text = device.name
+            address.text = device.address
+            bondState.text = when(device.bondState){
+                BluetoothDevice.BOND_BONDED -> context.getString(R.string.ble_bond_bonded)
+                BluetoothDevice.BOND_BONDING -> context.getString(R.string.ble_bond_bonding)
+                BluetoothDevice.BOND_NONE -> context.getString(R.string.ble_bond_none)
+                else -> ""
+            }
+        }
+    }
+}
+
+class DiffUtil: androidx.recyclerview.widget.DiffUtil.ItemCallback<BluetoothDevice>(){
+    override fun areItemsTheSame(oldItem: BluetoothDevice, newItem: BluetoothDevice): Boolean {
+        return (oldItem.address == newItem.address)
+    }
+
+    override fun areContentsTheSame(oldItem: BluetoothDevice, newItem: BluetoothDevice): Boolean {
+        return (oldItem.uuids == newItem.uuids)
+    }
+}
--- app/src/main/java/com/example/androidtechnote/ble/BleScanActivity.kt
+++ app/src/main/java/com/example/androidtechnote/ble/BleScanActivity.kt
@@ -10,12 +10,15 @@ import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Rect
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.util.TypedValue
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -23,7 +26,9 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import com.example.androidtechnote.R
+import com.example.androidtechnote.coordinator.bottomsheet.BottomSheetTodoAdapter
 import com.example.androidtechnote.databinding.ActivityBleScanBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -31,6 +36,8 @@ import kotlinx.coroutines.launch
 class BleScanActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityBleScanBinding
+
+    private val listAdapter = BleListAdapter()
 
     private fun PackageManager.missingSystemFeature(name: String): Boolean = !hasSystemFeature(name)
 
@@ -45,6 +52,11 @@ class BleScanActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_ble_scan)
 
         checkPermission()
+
+        binding.listRecycler.apply {
+            adapter = listAdapter
+            addItemDecoration(ItemTopDeco(5.dp))
+        }
     }
 
     override fun onResume() {
@@ -136,7 +148,7 @@ class BleScanActivity : AppCompatActivity() {
     }
 
     // 5. 블루투스 스캔/콜백
-    private val handler = Handler(Looper.myLooper()!!)
+    //private val handler = Handler(Looper.myLooper()!!)
     private var mScanning: Boolean = false
     private var arrayDevices = ArrayList<BluetoothDevice>()
 
@@ -144,16 +156,20 @@ class BleScanActivity : AppCompatActivity() {
         override fun onScanResult(callbackType: Int, result: ScanResult?) {
             super.onScanResult(callbackType, result)
             result?.let {
-                if (!arrayDevices.contains(it.device))
-                    arrayDevices.add(it.device)
+                if (result.device.name != null){
+                    if (!arrayDevices.contains(result.device))
+                        arrayDevices.add(result.device)
+                }
             }
         }
         override fun onBatchScanResults(results: MutableList<ScanResult>?) {
             super.onBatchScanResults(results)
             results?.let {
                 for (result in it){
-                    if (!arrayDevices.contains(result.device))
-                        arrayDevices.add(result.device)
+                    if (result.device.name != null){
+                        if (!arrayDevices.contains(result.device))
+                            arrayDevices.add(result.device)
+                    }
                 }
             }
         }
@@ -174,7 +190,7 @@ class BleScanActivity : AppCompatActivity() {
                     bluetoothAdapter.bluetoothLeScanner.stopScan(leScanCallback)
                     invalidateOptionsMenu()
 
-                    Log.d("스캔 완료","찾은 Device: ${arrayDevices.size}")
+                    listAdapter.submitList(arrayDevices)
                 }
 
                 // 미리 정의된 검색 기간 후 검색을 중지
@@ -206,4 +222,23 @@ class BleScanActivity : AppCompatActivity() {
         if (mScanning) scanLeDevice(false)
         startActivity(intent)
     }*/
+
+    val Int.dp: Int
+        get() {
+            val metrics = resources.displayMetrics
+            return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), metrics).toInt()
+        }
+
+    class ItemTopDeco(private val space: Int) :
+        RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+
+            val position = parent.getChildAdapterPosition(view) //각 아이템뷰의 순서 (index)
+            val totalItemCount = state.itemCount                //총 아이템 수
+            val scrollPosition = state.targetScrollPosition     //스크롤 됬을때 아이템 position
+
+            if (position != totalItemCount - 1)
+                outRect.bottom = space
+        }
+    }
 }
